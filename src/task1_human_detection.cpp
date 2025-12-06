da2c53c5fcefb3e59440f7927250f804209f8e23
huanminh1 <huan16122002@gmail.com>
2025-12-07 05:13:26 +0700

    add feature human detection

---
+++ src/task1_human_detection.cpp
@@ -0,0 +1,76 @@
+#include "task1_human_detection.h"
+void human_detection_task1(void *pvParameters){
+    MyLD2410 sensor(sensorSerial);
+    sensor.enhancedMode();
+    pinMode(10, OUTPUT);
+    // Khởi tạo UART theo chân đã cấu hình trong task_human_detection.h
+    sensorSerial.begin(LD2410_BAUD_RATE, SERIAL_8N1, LD2410_RX_PIN, LD2410_TX_PIN);
+    vTaskDelay(500/portTICK_PERIOD_MS); // Chờ một chút để UART ổn định
+
+    sensor.begin();
+    sensor.setNoOneWindow(3); // Thiết lập cửa sổ không có người là 3 giây
+    while (1)
+    {
+        if (sensor.check() == MyLD2410::Response::DATA){
+            Serial.print(sensor.statusString());
+            if (sensor.presenceDetected()) {
+
+                turnONLED(true);
+                Serial.print(", distance: ");
+                Serial.print(sensor.detectedDistance());  
+                Serial.print("cm");
+            } else {
+                
+                turnONLED(false);
+            }
+            Serial.println();
+
+            if (sensor.movingTargetDetected()) {
+                Serial.print(" MOVING    = ");
+                Serial.print(sensor.movingTargetSignal());
+                Serial.print("@");
+                Serial.print(sensor.movingTargetDistance());
+                Serial.print("cm ");
+                if (sensor.inEnhancedMode()) {
+                Serial.print("\n signals->[");
+                sensor.getMovingSignals().forEach(printValue);
+                Serial.print(" ] thresholds:");
+                sensor.getMovingThresholds().forEach(printValue);
+                }
+                Serial.println();
+            }
+            if (sensor.stationaryTargetDetected()) {
+                Serial.print(" STATIONARY= ");
+                Serial.print(sensor.stationaryTargetSignal());
+                Serial.print("@");
+                Serial.print(sensor.stationaryTargetDistance());
+                Serial.print("cm ");
+                if (sensor.inEnhancedMode()) {
+                Serial.print("\n signals->[");
+                sensor.getStationarySignals().forEach(printValue);
+                Serial.print(" ] thresholds:");
+                sensor.getStationaryThresholds().forEach(printValue);
+                }
+                Serial.println();
+            }
+            Serial.println();
+        }
+
+        vTaskDelay(300 / portTICK_PERIOD_MS); // Đợi 100ms trước khi đọc giá trị tiếp theo
+    }
+}
+
+void printValue(const byte &val) {
+  Serial.print(' ');
+  Serial.print(val);
+}
+
+void turnONLED(bool detected) {
+    int ledState = digitalRead(10);
+    int wantState = detected ? HIGH : LOW;
+    if (ledState != wantState) {
+        digitalWrite(10, wantState);
+        human_detected = wantState;
+    }
+    else return;
+}
